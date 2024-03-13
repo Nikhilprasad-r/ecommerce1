1f85d2019952ffcd6740af3cd4df9fdde41e68bb
Nikhil Prasad <nikhilprasadinbox@gmail.com>
2024-03-13 09:10:33 +0530

    day2

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,11 @@
 import Image from "next/image";
+import About from "./About";
+import TopProducts from "./Home/TopProducts";
 
 export default function Home() {
-  return <></>;
+  return (
+    <main>
+      <About />
+    </main>
+  );
 }
